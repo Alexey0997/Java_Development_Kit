6a211a968a0f0588d3f76795071cc25e1dfcd3b4
alexey <asivlev@yandex.ru>
2023-10-04 02:38:13 +0300

    ClientGUI is a message sending class

--- seminar_1/server/ClientGUI.java
+++ seminar_1/server/ClientGUI.java
@@ -1,7 +1,12 @@
-package ru.geekbrains.seminar_1;
+package ru.geekbrains.seminar_1.server;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.io.*;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 /**
  * Задача: Создать окно клиента чата. Окно должно содержать JtextField для ввода логина, пароля, IP-адреса сервера,
@@ -12,43 +17,76 @@ import java.awt.*;
  */
 
 public class ClientGUI extends JFrame {
-    private static final int WIDTH = 400;
-    private static final int HEIGHT = 300;
-    private final JTextArea log = new JTextArea();
-    private final JPanel panelTop = new JPanel(new GridLayout(2, 3));
-    private final JTextField tfIPAddress = new JTextField("127.0.0.1");
-    private final JTextField tfPort = new JTextField("8189");
-    private final JTextField tfLogin = new JTextField("Login");
-    private final JPasswordField tfPassword = new JPasswordField("12345");
-    private final JButton btnLogin = new JButton("Login");
+    private static final int HEIGHT = 340;
+    private static final int WIDTH = 350;
+    private static final int WINDOW_POS_X = 650;
+    private static final int WINDOW_POS_Y = 100;
 
-    private final JPanel panelBottom = new JPanel(new GridLayout());
-    private final JTextField tfMessage = new JTextField();
-    private final JButton btnSend = new JButton("Send");
 
-    ClientGUI() {
+    private static final String LOG_FILE = "chat_log.txt";
+    private final JTextArea textOutput = new JTextArea("");
+    private final JTextField textInput = new JTextField();
+
+    ClientGUI(String login) {
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
-        setSize(WIDTH, HEIGHT);
-        setTitle("Chat Client");
+        setTitle("CHAT CLIENT");
+        setBounds(WINDOW_POS_X, WINDOW_POS_Y, WIDTH, HEIGHT);
 
-        panelTop.add(tfIPAddress);
-        panelTop.add(tfPort);
-        panelTop.add(tfLogin);
-        panelTop.add(tfPassword);
-        panelTop.add(btnLogin);
-        add(panelTop, BorderLayout.CENTER);
-        panelBottom.add(btnSend, BorderLayout.EAST);
-        add(panelBottom, BorderLayout.SOUTH);
-        log.setEditable(false);
-        JScrollPane scrollLog = new JScrollPane(log);
-        add(scrollLog);
+        textOutput.setEditable(false);
+        textOutput.setBackground(Color.GRAY);
+
+        loadChatHistory();
+
+        JPanel grid = new JPanel(new GridLayout(4, 1));
+        grid.add(textOutput);
+        JLabel label = new JLabel("                             ВВЕДИТЕ СООБЩЕНИЕ ");
+        grid.add(label);
+        grid.add(textInput);
+        JButton buttonConnect = new JButton("ОТПРАВИТЬ");
+        grid.add(buttonConnect);
+
+        buttonConnect.addActionListener(e -> sendMessage(login));
+
+        textInput.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent e) {
+                if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+                    sendMessage(login);
+                }
+            }
+        });
+
+        add(grid);
         setVisible(true);
     }
 
-    public static void main(String[] args){
-        new ClientGUI();
+    private void sendMessage(String login) {
+        String message = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yy/MM/dd HH:mm:ss")) + " " + login + " : " + textInput.getText() + "\n";
+        textOutput.append(message);
+        saveMessageToFile(message);
+        textInput.setText("");
     }
 
+    private void saveMessageToFile(String message) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(LOG_FILE, true))) {
+            writer.write(message);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
+    private void loadChatHistory() {
+        File file = new File(LOG_FILE);
+        if (file.exists()) {
+            try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    textOutput.append(line + "\n");
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }

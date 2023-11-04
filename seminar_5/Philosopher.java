4e33c3473365c231a88e97362a7e8a0b30cb6665
alexey <asivlev@yandex.ru>
2023-11-04 16:13:30 +0300

    Philosopher is a class that implements the Runnable interface.

--- seminar_5/Philosopher.java
+++ seminar_5/Philosopher.java
@@ -1,13 +1,21 @@
-package ru.geekbrains.test;
+package ru.geekbrains.seminar_5;
 
-
+/**
+ * Philosopher - класс реализующий интерфейс Runnable.
+ */
 public class Philosopher implements Runnable {
-    private int eating = 3;
-    private final Fork leftFork;
-    private final Fork rightFork;
-    private final String name;
-    private final long timeout;
+    private int eating = 3;        // Количество приемов пищи.
+    private final Fork leftFork;   // Левая вилка.
+    private final Fork rightFork;  // Правая вилка.
+    private final String name;     // Имя философа.
+    private final long timeout;    // Время выполнения действия.
 
+    /**
+     * Конструктор класса, в котором:
+     * @param leftFork  - левая вилка.
+     * @param rightFork - правая вилка.
+     * @param name      - имя философа.
+     */
     public Philosopher(Fork leftFork, Fork rightFork, String name) {
         this.leftFork = leftFork;
         this.rightFork = rightFork;
@@ -15,7 +23,15 @@ public class Philosopher implements Runnable {
         timeout = System.nanoTime();
     }
 
-
+    /**
+     * Класс run реализует алгоритм, в соответствии с которым философы думают,
+     * затем первый из них берет две вилки и начинает прием пищи. Один из последующих философов,
+     * который имеет возможность взять две вилки, также берет их и приступает к приему пищи. Остальные - думают.
+     * Завершив трапезу, философы кладут вилки и начинают думать. Их соседи, получившие возможность взять по две вилки,
+     * берут их и начинают кушать.
+     * После трех приемов пищи у каждого участника наступает чувство сытости.
+     * Когда наелись все, программа прекращает работу.
+     */
     @Override
     public void run() {
         int count = 0;
@@ -28,7 +44,7 @@ public class Philosopher implements Runnable {
             synchronized (rightFork) {
                 synchronized (leftFork) {
                     try {
-                        activity("взял вилки " + leftFork.amount + " и " + rightFork.amount, 500);
+                        activity("взял вилки " + leftFork.number + " и " + rightFork.number, 500);
                     } catch (InterruptedException e) {
                         throw new RuntimeException(e);
                     }
@@ -40,7 +56,7 @@ public class Philosopher implements Runnable {
                     eating--;
                     count++;
                     try {
-                        activity("положил вилки " + leftFork.amount + " и " + rightFork.amount, 500);
+                        activity("положил вилки " + leftFork.number + " и " + rightFork.number, 500);
                     } catch (InterruptedException e) {
                         throw new RuntimeException(e);
                     }
@@ -50,50 +66,6 @@ public class Philosopher implements Runnable {
         activity("покушал " + count + " раза и наелся.\n" );
     }
 
-
-
-//
-//    @Override
-//    public void run() {
-//        while (eating > 0) {
-//            try {
-//                activity("думает", 3000);
-//            } catch (InterruptedException e) {
-//                throw new RuntimeException(e);
-//            }
-//            synchronized (rightFork) {
-//                try {
-//                    activity("взял вилку " + rightFork.amount, 500);
-//                } catch (InterruptedException e) {
-//                    throw new RuntimeException(e);
-//                }
-//                synchronized (leftFork) {
-//                    try {
-//                        activity("взял вилку " + leftFork.amount, 500);
-//                    } catch (InterruptedException e) {
-//                        throw new RuntimeException(e);
-//                    }
-//                    try {
-//                        activity("кушает ", 2000);
-//                    } catch (InterruptedException e) {
-//                        throw new RuntimeException(e);
-//                    }
-//                    eating--;
-//                    try {
-//                        activity("положил вилку " + leftFork.amount, 500);
-//                    } catch (InterruptedException e) {
-//                        throw new RuntimeException(e);
-//                    }
-//                }
-//                try {
-//                    activity("положил вилку " + rightFork.amount, 500);
-//                } catch (InterruptedException e) {
-//                    throw new RuntimeException(e);
-//                }
-//            }
-//        }
-//        activity("философ сыт");
-//    }
 
     private void activity(String act, long timeout) throws InterruptedException {
         activity(act);
